62d7ccedc6cf40d9dfd7d2b7de80fe66e8c46dab
gabriele <gabriele.dipinto@gmail.com>
2024-11-10 22:09:09 +0100

    framer motion, img courses

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,60 +1,167 @@
 // src/pages/HomePage.js
 import React from 'react';
-import { Container, Typography, Box} from '@mui/material';
+import { Container, Typography, Button, Box, Card, CardContent, CardMedia } from '@mui/material';
 import Grid from '@mui/material/Grid2';
+import { motion } from 'framer-motion';
 import ParallaxSection from '../components/ParallaxSection';
+import {Link} from 'react-router-dom';
 
-// 
+const corsi = [
+  { title: 'Architettura del Computer', description: 'Componenti di base e funzionamento di un elaboratore', link: '/corsi/architettura-del-computer', image: '/images/architettura.jpg' },
+  { title: 'Programmazione', description: 'Introduzione ai concetti di programmazione e linguaggi di base.', link: 'corsi/programmazione', image: '/images/programmazione.jpg' },
+  { title: 'Web Development', description: 'Creazione di siti web moderni utilizzando HTML, CSS e JavaScript.', link: '/corsi/web-development', image: '/images/webdev.jpg'},
+  { title: 'Database', description: 'Fondamenti di progettazione e gestione di database relazionali.', link: '/corsi/database', image: '/images/database.png'},
+  { title: 'Sicurezza Informatica', description: 'Comprendere le basi della sicurezza informatica e delle reti.', link: '/corsi/sicurezza-informatica', image: '/images/cybersecurity.jpg' },
+];
 
 const HomePage = () => {
   return (
-    <Container style={{ padding: '20px' }}>
-        <div>
+    <Container maxWidth="lg">
         <ParallaxSection  bgImage="/images/immaginecomputer.jpg" title="Benvenuto nel Digital Lab" />
         <div style={{ padding: '5px' }}>
         </div>
-      <Box component="main" sx={{ my: 4 }}>
-        <Typography variant="h5" gutterBottom textAlign={'center'}>
-          Benvenuto! Pronto a cominciare?
-        </Typography>
-        <Grid container spacing={10} alignItems="center">
-            <Grid size={{ xs: 6, md: 4, sm: 12 }}>
-              <img src='/images/iconapc.png' alt="Icona computer" style={{ width: '100%', height: 'auto' }} />
-            </Grid>
-            <Grid size={{ xs: 6, md: 8, sm: 12 }}>
-                <Typography>
-                    Il <strong>Digital Lab</strong> è un progetto creato per ampliare le conoscenze informatiche degli studenti delle scuole secondarie.
-                    Troverai numerose risorse, dai programmi di Informatica scolastici fino a progetti ideati appositamente per gli studenti. Sono risorse totalmente <strong>gratuite</strong>, quindi non sprecarle!
-                    <ul>
-                        <li>Dispense</li>
-                        <li>Progetti formativi</li>
-                        <li>Esercitazioni</li>
-                        <li>Slide Riassuntive</li>
-                    </ul>
-                    
-                </Typography>
+    
+      {/* Hero Section*/}
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: { xs: 'column', md: 'row' },
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            py: 6,
+            backgroundColor: '#f5f5f5',
+            padding: 4,
+            borderRadius: 2,
+          }}
+        >
+          <Box sx={{ flex: 1, textAlign: { xs: 'center', md: 'left' } }}>
+            <Typography variant="h5" gutterBottom>
+             Pronto a cominciare?
+            </Typography>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+            Il <strong>Digital Lab</strong> è un progetto creato per ampliare le conoscenze informatiche degli studenti delle scuole secondarie.
+            Troverai numerose risorse, dai programmi di Informatica scolastici fino a progetti ideati appositamente per gli studenti. Sono risorse totalmente <strong>gratuite</strong>, quindi non sprecarle!
+            </Typography>
+            <Button variant="contained" color="primary" sx={{ mt: 2, mr: 2 }}>
+            Vai ai corsi
+            </Button>
+          </Box>
+          <Box sx={{ flex: 1, mt: { xs: 4, md: 0 } }}>
+            <motion.img
+              src="/images/iconapc.png"
+              alt="Hero"
+              style={{ width: '100%', height: 'auto', borderRadius: 8 }}
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1, delay: 0.3 }}
+            />
+          </Box>
+        </Box>
+      </motion.div>
 
-            </Grid>
-            
-        </Grid>
-        <Box sx={{ my: 4 }}>
-        <Grid container spacing={10} alignItems="center">
-        <Grid size={{ xs: 6, md: 8, sm: 12 }}>
-          <Typography variant="h5">Chi sono?</Typography>
-          <Typography>
-            Sono Gabriele Di Pinto, docente di Laboratorio di Scienze Informatiche. Nella vita mi occupo di digitale, innovazione e business online, oltre che di formazione per ragazzi, adulti e aziende. Ho creato il Digital Lab per fornire ai miei studenti un luogo dove studiare, esercitarsi e crescere. Insegnare non è soltanto un mestiere, ma una passione per me.
+      {/* Courses Section*/}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.2, delay: 0.5 }}
+      >
+        <Box sx={{ mt: 6 }}>
+          <Typography variant="h5" gutterBottom align="center">
+            I Corsi
           </Typography>
-        </Grid>
-        <Grid size={{ xs: 6, md: 4, sm: 12 }}>
-                <img src='/images/immaginegabry.png' alt="Descrizione dell'immagine" style={{ width: '100%', height: 'auto' }} />
-        </Grid>
-        </Grid>
+          <Grid container spacing={4} justifyContent="center">
+            {corsi.map((corso, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <motion.div
+                  whileHover={{ scale: 1.05 }}
+                  transition={{ type: 'spring', stiffness: 300 }}
+                >
+                  <Card sx={{ height: 300, width: 300 }} align='center'>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={corso.image}
+                      alt={corso.title}
+                    />
+                    <CardContent>
+                      <Typography variant="h6" component="div">
+                        {corso.title}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {corso.description}
+                      </Typography>
+                    </CardContent>
+                    <Button 
+                      variant="contained" 
+                      color="primary" 
+                      sx={{ m: 2 }}
+                      component={Link}
+                      to={corso.link}
+                    >
+                      Vai al corso
+                    </Button>
+                  </Card>
+                </motion.div>
+              </Grid>
+            ))}
+          </Grid>
         </Box>
+      </motion.div>
+
+
+      {/* Presentation Section*/}
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: { xs: 'column', md: 'row' },
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            py: 6,
+            backgroundColor: '#f5f5f5',
+            padding: 8,
+            borderRadius: 2,
+          }}
+        >
+
+            <Box sx={{ flex: 1, mt: { xs: 4, md: 0 } }}>
+              <motion.img
+                src="/images/immaginegabry.png"
+                alt="immagine gabriele"
+                style={{ width: '70%', height: '70%', borderRadius: 8 }}
+                initial={{ opacity: 0, scale: 0.9 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 1, delay: 0.3 }}
+              />
+            </Box>
+            <Box sx={{ flex: 1, textAlign: { xs: 'center', md: 'left' } }}>
+            <Typography variant="h5" gutterBottom>
+             Chi sono?
+            </Typography>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+            Sono Gabriele Di Pinto, docente di Laboratorio di Scienze Informatiche. Nella vita mi occupo di digitale, innovazione e business online, oltre che di formazione per ragazzi, adulti e aziende. Ho creato il Digital Lab per fornire ai miei studenti un luogo dove studiare, esercitarsi e crescere. Insegnare non è soltanto un mestiere, ma una passione per me.
+            </Typography>
+            <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+              Contattami!
+            </Button>
+          </Box>
         </Box>
-      </div>
+      </motion.div>
+
+
     </Container>
+
+    
   );
 };
 
 export default HomePage;
-
--- src/pages/corsi/MainCorsi.js
+++ src/pages/corsi/MainCorsi.js
@@ -1,48 +1,69 @@
 // src/pages/MainCorsi.js
 import React from 'react';
-import { Container, Card, CardContent, CardActions, Typography, Button } from '@mui/material';
+import { Container, Card, CardContent, CardMedia, Typography, Button, Box } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import {Link} from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const corsi = [
-    { title: 'Architettura del Computer', description: 'Componenti di base e funzionamento di un elaboratore', link: '/corsi/architettura-del-computer' },
-    { title: 'Programmazione', description: 'Introduzione ai concetti di programmazione e linguaggi di base.', link: 'corsi/programmazione' },
-    { title: 'Web Development', description: 'Creazione di siti web moderni utilizzando HTML, CSS e JavaScript.', link: '/corsi/web-development'},
-    { title: 'Database', description: 'Fondamenti di progettazione e gestione di database relazionali.', link: '/corsi/database'},
-    { title: 'Sicurezza Informatica', description: 'Comprendere le basi della sicurezza informatica e delle reti.', link: '/corsi/sicurezza-informatica' },
+  { title: 'Architettura del Computer', description: 'Componenti di base e funzionamento di un elaboratore', link: '/corsi/architettura-del-computer', image: '/images/architettura.jpg' },
+  { title: 'Programmazione', description: 'Introduzione ai concetti di programmazione e linguaggi di base.', link: 'corsi/programmazione', image: '/images/programmazione.jpg' },
+  { title: 'Web Development', description: 'Creazione di siti web moderni utilizzando HTML, CSS e JavaScript.', link: '/corsi/web-development', image: '/images/webdev.jpg'},
+  { title: 'Database', description: 'Fondamenti di progettazione e gestione di database relazionali.', link: '/corsi/database', image: '/images/database.png'},
+  { title: 'Sicurezza Informatica', description: 'Comprendere le basi della sicurezza informatica e delle reti.', link: '/corsi/sicurezza-informatica', image: '/images/cybersecurity.jpg' },
 ];
 
 const MainCorsi = () => {
   return (
     <Container maxWidth="lg" sx={{ mt: 4 }}>
-      <Typography variant="h5" gutterBottom align="center">
-        Corsi e dispense
-      </Typography>
-      <Grid container spacing={4} alignItems="center" justifyContent="center">
-        {corsi.map((corso, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index} sx={{ display: 'flex' }}>
-            <Card sx={{ height: 300, width: 300 }}>
-              <CardContent>
-                <Typography variant="h6" component="div">
-                  {corso.title}
-                </Typography>
-                <Typography variant="body2" color="text.primary">
-                  {corso.description}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button 
-                    component={Link}
-                    to={corso.link}
-                    size="small"
-                    color="primary">
-                  Scopri di più
-                </Button>
-              </CardActions>
-            </Card>
+      {/* Courses Section*/}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.2, delay: 0.5 }}
+      >
+        <Box sx={{ mt: 6 }}>
+          <Typography variant="h5" gutterBottom align="center">
+            I Corsi
+          </Typography>
+          <Grid container spacing={4} justifyContent="center">
+            {corsi.map((corso, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <motion.div
+                  whileHover={{ scale: 1.05 }}
+                  transition={{ type: 'spring', stiffness: 300 }}
+                >
+                  <Card sx={{ height: 300, width: 300 }} align='center'>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={corso.image}
+                      alt={corso.title}
+                    />
+                    <CardContent>
+                      <Typography variant="h6" component="div">
+                        {corso.title}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {corso.description}
+                      </Typography>
+                    </CardContent>
+                    <Button 
+                      variant="contained" 
+                      color="primary" 
+                      sx={{ m: 2 }}
+                      component={Link}
+                      to={corso.link}
+                    >
+                      Vai al corso
+                    </Button>
+                  </Card>
+                </motion.div>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </Box>
+      </motion.div>
     </Container>
   );
 };
